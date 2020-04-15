bde12651e069b65136fd249bff02fdfa0219a19b
Nathan Shepherd <your@email.goes.here>
2020-04-15 19:14:38 +0200

    build update

---
+++ build/precache-manifest.d6c15c3cb37762aaa0c188aabcc4a3cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4595652b12930d2b6aee9fed82d047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b65107fe40f8ca4aeef",
+    "url": "/static/js/2.1b86f78b.chunk.js"
+  },
+  {
+    "revision": "fff893798278dc57ce07f24dd8c43a2b",
+    "url": "/static/js/2.1b86f78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bfee23637e1471c396",
+    "url": "/static/js/main.fce71dce.chunk.js"
+  },
+  {
+    "revision": "945bbeb1341db8bfff7c",
+    "url": "/static/js/runtime-main.cce5275e.js"
+  },
+  {
+    "revision": "7cb637638a22ac4114fd07133fa3d0c5",
+    "url": "/static/media/Web-SEO-Online-Logo-white-transparent.7cb63763.png"
+  }
+]);
